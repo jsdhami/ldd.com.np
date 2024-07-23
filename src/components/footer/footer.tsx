295a9c81535edb991a33c13db20ebea859e36128
Janak Singh Dhami <139155991+jsdhami@users.noreply.github.com>
2024-07-23 09:39:30 +0000

    added pdf Viewer_some new

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,9 +5,9 @@ const footer = () => {
   return (
    <>
  <div className="flex justify-around items-center p-1 bg-gray-800 text-white">
-      <div className="text-sm font-medium"><Link href="/">© 2024 LDD Notes. All rights reserved</Link></div>
+      <div className="text-sm font-medium"><Link href="/">© 2024 LDD Notes.</Link> All rights reserved.</div>
       <div className="flex items-center space-x-4">
-            <Link href="#" className="hover:text-gray-300">Home</Link>
+            <Link href="#" className="hover:text-gray-300">Contact</Link>
             <Link href="#" className="hover:text-gray-300">Notes</Link>
             <Link href="#" className="hover:text-gray-300">About</Link>
       </div>

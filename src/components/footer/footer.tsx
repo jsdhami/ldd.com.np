355b7732afd37b8b876db6c4b512419a79ef1d04
Janak Singh Dhami <139155991+jsdhami@users.noreply.github.com>
2024-07-24 07:43:59 +0000

    update

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,12 +4,15 @@ import React from 'react'
 const footer = () => {
   return (
    <>
- <div className="flex justify-around items-center p-1 bg-gray-500 text-white">
-      <div className="text-sm font-medium"><Link href="/">© 2024 LDD Notes.</Link> All rights reserved.</div>
-      <div className="flex items-center space-x-4">
-            <Link href="#" className="hover:text-gray-300">Contact</Link>
-            <Link href="#" className="hover:text-gray-300">Notes</Link>
-            <Link href="#" className="hover:text-gray-300">About</Link>
+ <div className="flex justify-around items-center p-2 bg-slate-200 text-gray-800 ">
+      <div className="text-sm font-medium text-wrap">©2024 <Link className='font-bold' href="/">LDD Notes.</Link> All rights reserved.</div>
+      <div className="hidden md:flex items-center space-x-4">
+            <Link href="#" className="hover:text-gray-600">Contact</Link>
+            <Link href="#" className="hover:text-gray-600">Notes</Link>
+            <Link href="#" className="hover:text-gray-600">About</Link>
+      </div>
+      <div className='text-sm font-medium'>
+          <div className="text-sm font-medium text-wrap ">Developed by <Link className='font-bold hover:text-gray-600' href='https://dev.jsdhami.com.np'>Janak S. Dhami</Link> </div>
       </div>
 </div>     
    </>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,12 +4,16 @@ import React from 'react'
 const header = () => {
   return (
  <>
-      <div className="flex justify-between items-center px-8 py-4 bg-gray-600 text-white">
-      <div className="text-2xl font-bold"><Link href="/">LDD Notes</Link></div>
+      <div className="flex justify-around items-center py-3 bg-gray-200 text-slate-700">
+      <div className="text-2xl font-bold"><Link href="/">LDD NOTES</Link></div>
       <div className="flex items-center space-x-4">
-            <Link href="#" className="hover:text-gray-300">Home</Link>
-            <Link href="#" className="hover:text-gray-300">Notes</Link>
-            <Link href="#" className="hover:text-gray-300">About</Link>
+            <Link href="#" className="hover:text-gray-600">Home</Link>
+            <Link href="#" className="hover:text-gray-600">Notes</Link>
+            <Link href="#" className="hover:text-gray-600">About</Link>
+      </div>
+      {/* get start button */}
+      <div className="flex items-center space-x-4 rounded-full bg-black py-2 px-4 shadow-sm">
+            <Link href="#" className="text-slate-200 hover:text-gray-300">Get Started</Link>
       </div>
       </div>
  </>

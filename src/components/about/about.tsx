88e3eddc337d80dda8bd9291a88d68fee642b1ca
Janak Singh Dhami <139155991+jsdhami@users.noreply.github.com>
2024-07-25 17:54:27 +0000

    a

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,40 +1,50 @@
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable"
+// import React from 'react'
+// import {
+//   ResizableHandle,
+//   ResizablePanel,
+//   ResizablePanelGroup,
+// } from "@/components/ui/resizable"
+
+// const about = () => {
+//   return (
+//     <>
+//     <ResizablePanelGroup
+//       direction="horizontal"
+//       className="min-w-full border"
+//     >
+//       <ResizablePanel defaultSize={25}>
+//         <div className="flex h-screen items-center justify-center p-6">
+//           <span className="font-semibold">One</span>
+//         </div>
+//       </ResizablePanel>
+//       <ResizableHandle />
+//       <ResizablePanel defaultSize={75}>
+//         <ResizablePanelGroup direction="vertical">
+//           <ResizablePanel defaultSize={25}>
+//             <div className="flex h-full items-center justify-center p-6">
+//               <span className="font-semibold">Two</span>
+//             </div>
+//           </ResizablePanel>
+//           <ResizableHandle />
+//           <ResizablePanel defaultSize={75}>
+//             <div className="flex h-full items-center justify-center p-6">
+//               <span className="font-semibold">Three</span>
+//             </div>
+//           </ResizablePanel>
+//         </ResizablePanelGroup>
+//       </ResizablePanel>
+//     </ResizablePanelGroup>
+//     </>
+//   )
+// }
+
+// export default about
 
 import React from 'react'
+
 const about = () => {
   return (
-    <>
-    <ResizablePanelGroup
-      direction="horizontal"
-      className="min-w-full border"
-    >
-      <ResizablePanel defaultSize={25}>
-        <div className="flex h-screen items-center justify-center p-6">
-          <span className="font-semibold">One</span>
-        </div>
-      </ResizablePanel>
-      <ResizableHandle />
-      <ResizablePanel defaultSize={75}>
-        <ResizablePanelGroup direction="vertical">
-          <ResizablePanel defaultSize={25}>
-            <div className="flex h-full items-center justify-center p-6">
-              <span className="font-semibold">Two</span>
-            </div>
-          </ResizablePanel>
-          <ResizableHandle />
-          <ResizablePanel defaultSize={75}>
-            <div className="flex h-full items-center justify-center p-6">
-              <span className="font-semibold">Three</span>
-            </div>
-          </ResizablePanel>
-        </ResizablePanelGroup>
-      </ResizablePanel>
-    </ResizablePanelGroup>
-    </>
+    <div>about</div>
   )
 }
 

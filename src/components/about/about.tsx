af4791e5292a490cca9eecc55b3d68274c7aac75
Janak Singh Dhami <139155991+jsdhami@users.noreply.github.com>
2024-09-10 02:26:34 +0545

    Update about.tsx

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const About = () => {
+const about = () => {
   return (
     <div className="max-w-4xl mx-auto py-10 px-4">
       <h1 className="text-3xl font-bold mb-6 text-center">About LDD Notes</h1>
@@ -20,4 +20,4 @@ const About = () => {
   );
 }
 
-export default About;
+export default about;
